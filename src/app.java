0b133e06f54ea880906a1ddece7291d41f9c207a
WidoMartinez <widomartinez@gmail.com>
2022-11-07 20:50:55 -0300

    en la pega

--- src/app.java
+++ src/app.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class app {
     public static void main(String[] args) {
-        System.out.println("Hola como sd sd es dftas df");
+
+        Scanner scanner = new Scanner(System.in);
+        String[] userName = new String[2];
+        String[] passWord = new String[2];
+
+        System.out.println("Escriba su nombre usuario 1");
+        userName[0] = scanner.next();
+
+        System.out.println("Escriba su nombre usuario 2");
+        userName[1] = scanner.next();
+
+        System.out.println("Tu nombre es " + userName[0] + " " + userName[1]);
+
+        scanner.close();//puede no ir
     }
 }
