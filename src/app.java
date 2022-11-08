9578fd2fb662a0c6e6abb007e2ac6e02fdd4ee13
WidoMartinez <widomartinez@gmail.com>
2022-11-08 16:40:28 -0300

    dos

--- src/app.java
+++ src/app.java
@@ -5,7 +5,7 @@ public class app {
 
         Scanner scanner = new Scanner(System.in);
         String[] userName = new String[2];
-        String[] passWord = new String[2];
+        //String[] passWord = new String[2];
 
         System.out.println("Escriba su nombre usuario 1");
         userName[0] = scanner.next();
@@ -15,6 +15,6 @@ public class app {
 
         System.out.println("Tu nombre es " + userName[0] + " " + userName[1]);
 
-        scanner.close();//puede no ir
+        scanner.close();// puede no ir
     }
 }
