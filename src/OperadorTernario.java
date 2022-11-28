977559822e4a303e54a7f138ef2c6bd4a6ae1d20
WidoMartinez <widomartinez@gmail.com>
2022-11-28 18:44:59 -0300

    cambioi

---
+++ src/OperadorTernario.java
@@ -0,0 +1,9 @@
+public class OperadorTernario {
+
+    public static void main(String[] args) {
+        String variable = "";
+        System.out.println(variable);
+        variable = "aprobado";
+        System.out.println(variable);
+    }
+}
